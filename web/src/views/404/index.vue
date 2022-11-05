72e4ae65caf4495abdd40827a7358f3e205202dc
cyantiz <nhan053qt@gmail.com>
2022-11-05 21:48:47 +0700

    feat(web): add 404 page

---
+++ web/src/views/404/index.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { Icon } from "@iconify/vue"
+defineProps<{}>()
+</script>
+
+<template>
+    <div class="404 flex flex-col justify-center items-center w-screen h-screen gap-8">
+        <img src="/granny-smith-logo.svg" alt="logo" class="w-[96px]" />
+        <div class="uppercase font-bold text-lg flex items-center justify-center gap-4">
+            <span>404</span>
+            <span class="separate-line"></span>
+            <span>Page not found</span>
+        </div>
+        <RouterLink
+            :to="{ name: 'Home' }"
+            class="h-10 font-bold w-full max-w-[400px] bg-white border-black border-2 flex justify-center items-center text-[16px] cursor-pointer hover:text-primary hover:border-primary transition-all duration-300 group"
+        >
+            <span class="transition-all translate-x-2 group-hover:-translate-x-2">Trở về trang chủ</span>
+            <Icon icon="ph:arrow-right-bold" class="opacity-0 -translate-x-6 group-hover:-translate-x-1 group-hover:opacity-100 transition-all" />
+        </RouterLink>
+    </div>
+</template>
+
+<style lang="less" scoped>
+.separate-line {
+    @apply bg-slate-600 h-2/3 whitespace-nowrap;
+    width: 2px;
+    &__link {
+        @apply flex items-center gap-1 whitespace-nowrap;
+    }
+}
+</style>
