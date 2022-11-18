796b518322aff2a1e9dc549264b75b729e29193e
Tran Thi Hong Ngoc <73293612+rubyy2012@users.noreply.github.com>
2022-11-18 23:58:22 +0700

    feat(api): create model in DRF

---
+++ api/shoesshop/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ShoesshopConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'shoesshop'
---
+++ api/shoesshop/models.py
@@ -0,0 +1,93 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+    
+class Product(models.Model):
+    product_id = models.AutoField(primary_key=True)
+    name= models.CharField(max_length=100)
+    price = models.FloatField()
+    salePercent =  models.FloatField()
+    in_stock = models.PositiveBigIntegerField()
+    sold = models.PositiveBigIntegerField()
+class Image(models.Model):
+    image_id = models.AutoField(primary_key=True)
+    product= models.ForeignKey(Product,on_delete=models.CASCADE)
+    is_thumbnail = models.BooleanField()
+    url = models.URLField()
+    
+class Shoe(models.Model):
+    GENDER = [
+    (0, 'Male'),
+    (1, 'Female'),
+    (2, 'Both'),
+    ]
+
+    shoe_id = models.BigAutoField(primary_key=True)
+    product= models.ForeignKey(Product,on_delete=models.CASCADE)
+    gender = models.IntegerField(choices=GENDER,default=0)
+    series = models.CharField(max_length=100)
+    shape = models.BooleanField()
+
+class ShoeChild(models.Model):
+    shoe_child_id = models.BigAutoField(primary_key=True)
+    shoe= models.ForeignKey(Shoe,on_delete=models.CASCADE)
+    in_stock = models.PositiveBigIntegerField()
+    size = models.PositiveBigIntegerField()
+
+
+class Accessory(models.Model):
+    CATEGORIES = [
+    (0, 'Shock'),
+    (1, 'Tote'),
+    (2, 'Backpack'),
+    (3, 'Shoelace'),
+    ]
+
+    accessory_id = models.AutoField(primary_key=True)
+    product= models.ForeignKey(Product,on_delete=models.CASCADE)
+    category = models.IntegerField(choices=CATEGORIES,default=0)
+
+class Clothes(models.Model):
+    CATEGORIES = [
+    (0, 'Tee'),
+    (1, 'Hoodie'),
+    (2, 'Sweatshirt'),
+    ]
+     
+    clothes_id =  models.AutoField(primary_key=True)
+    product= models.ForeignKey(Product,on_delete=models.CASCADE)
+    category = models.IntegerField(choices=CATEGORIES,default=0)
+    
+class Order(models.Model):
+    STATUS = [
+    (0, 'in progress'),
+    (1, 'shipping'),
+    (2, 'done'),
+    ]
+    order_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    address = models.CharField(max_length=100)
+    province_id = models.PositiveBigIntegerField()
+    district_id = models.PositiveBigIntegerField()
+    commune_id = models.PositiveBigIntegerField()
+    status = models.IntegerField(choices=STATUS,default=0)
+
+class OrderProduct(models.Model):
+    order_product_id = models.AutoField(primary_key=True)
+    order= models.ForeignKey(Order,on_delete=models.CASCADE)
+    product= models.ForeignKey(Product,on_delete=models.CASCADE)
+    quantity = models.PositiveBigIntegerField()
+
+class UserLoveProduct(models.Model):
+    user_love_product_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    product= models.ForeignKey(Product,on_delete=models.CASCADE)
+    
+class UserCartProduct(models.Model):
+    user_cart_product_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    product= models.ForeignKey(Product,on_delete=models.CASCADE)
