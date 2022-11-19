119f9e06ff0f8fc282e424c6769eeab9c6c329d0
Tran Thi Hong Ngoc <73293612+rubyy2012@users.noreply.github.com>
2022-11-19 10:20:40 +0700

    feat(api): create model in DRF (#6)

---
+++ api/shoesshop/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+
+from shoesshop.models import Accessory, Clothes, Image, Order, OrderProduct, Product, Shoe, ShoeChild, UserCartProduct, UserLoveProduct
+
+# Register your models here.
+admin.site.register(Product)
+admin.site.register(Image)
+admin.site.register(Shoe)
+admin.site.register(ShoeChild)
+admin.site.register(Accessory)
+admin.site.register(Clothes)
+admin.site.register(Order)
+admin.site.register(OrderProduct)
+admin.site.register(UserLoveProduct)
+admin.site.register(UserCartProduct)
