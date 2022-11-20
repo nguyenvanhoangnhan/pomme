81c75707712aae87614efdaef494f6ab36352c36
letronghoangminh <letronghoangminh@gmail.com>
2022-11-20 10:36:38 +0700

    feat(web): dockerize vuejs application

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -7,6 +7,9 @@ import { AntDesignVueResolver } from "unplugin-vue-components/resolvers"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    server: {
+        port: 5173
+    },
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
