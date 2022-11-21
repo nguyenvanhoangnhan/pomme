eb949ec3e44dd68281b6dac2cbcf8cfc79c374e4
psycholog1st <50044415+letronghoangminh@users.noreply.github.com>
2022-11-21 21:42:58 +0700

    feature(api): Add sentry configuration (#22)

--- api/api/settings.py
+++ api/api/settings.py
@@ -9,12 +9,31 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
-from datetime import timedelta
-from pathlib import Path
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
+import sentry_sdk
+
+from datetime import timedelta
+from pathlib import Path
+from sentry_sdk.integrations.django import DjangoIntegration
+
+
+sentry_sdk.init(
+    dsn="https://0f46d5fb566c4a239bf41011045ba42e@o4504197595136000.ingest.sentry.io/4504197597691904",
+    integrations=[
+        DjangoIntegration(),
+    ],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
