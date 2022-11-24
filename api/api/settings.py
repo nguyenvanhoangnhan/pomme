6e96fc1371406f8add77a6dcd8689ef33d3e43a4
letronghoangminh <letronghoangminh@gmail.com>
2022-11-24 21:02:43 +0700

    fix(api): Add default secret key for Django

--- api/api/settings.py
+++ api/api/settings.py
@@ -45,7 +45,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("DJANGO_SECRET")
+SECRET_KEY = os.getenv("DJANGO_SECRET", "unsecure_key_for_django")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
