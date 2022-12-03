6282f0507ff2c5fa91c066cc16d5bf5972eb0988
letronghoangminh <letronghoangminh@gmail.com>
2022-12-03 22:08:18 +0700

    fix: fix lint

--- api-laravel/app/Http/Middleware/Logging.php
+++ api-laravel/app/Http/Middleware/Logging.php
@@ -1,16 +1,15 @@
 <?php
 
-
 namespace App\Http\Middleware;
 
-use Illuminate\Support\Facades\Log;
 use Closure;
+use Illuminate\Support\Facades\Log;
 
 class Logging
 {
     public function handle($request, Closure $next)
     {
-        $msg = join(" ", array($request->header('X-Forwarded-For'), $request->url(), $request->method(), $request->server('HTTP_USER_AGENT'))); 
+        $msg = join(' ', [$request->header('X-Forwarded-For'), $request->url(), $request->method(), $request->server('HTTP_USER_AGENT')]);
         Log::build([
             'driver' => 'single',
             'path' => storage_path('logs/app.log'),
