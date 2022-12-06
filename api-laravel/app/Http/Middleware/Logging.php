0a80d9b19ea6078ea8b84378d3fc48d4f2ba11b8
letronghoangminh <letronghoangminh@gmail.com>
2022-12-06 23:41:39 +0700

    fix(api-laravel): fix lint for camelCase

--- api-laravel/app/Http/Middleware/Logging.php
+++ api-laravel/app/Http/Middleware/Logging.php
@@ -9,11 +9,11 @@ class Logging
 {
     public function handle($request, Closure $next)
     {
-        $client_ip = $request->header('X-Forwarded-For');
-        $request_method = $request->method();
-        $request_url = $request->url();
-        $user_agent = $request->server('HTTP_USER_AGENT');
-        $msg = "Request coming {'client-IP': '$client_ip', 'request-method': '$request_method', 'url': '$request_url', 'user-agent': '$user_agent'}";
+        $clientIp = $request->header('X-Forwarded-For');
+        $requestMethod = $request->method();
+        $requestUrl = $request->url();
+        $userAgent = $request->server('HTTP_USER_AGENT');
+        $msg = "Request coming {'client-IP': '$clientIp', 'request-method': '$requestMethod', 'url': '$requestUrl', 'user-agent': '$userAgent'}";
         Log::build([
             'driver' => 'single',
             'path' => storage_path('logs/app.log'),
