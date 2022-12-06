696d315f9f1a530d4be7908068abbb73608ec173
letronghoangminh <letronghoangminh@gmail.com>
2022-12-06 23:40:03 +0700

    feat(api-laravel): Add log for all requests

--- api-laravel/app/Http/Middleware/Logging.php
+++ api-laravel/app/Http/Middleware/Logging.php
@@ -13,7 +13,7 @@ class Logging
         $request_method = $request->method();
         $request_url = $request->url();
         $user_agent = $request->server('HTTP_USER_AGENT');
-        $msg = "{'client-IP': '$client_ip', 'request-method': '$request_method', 'url': '$request_url', 'user-agent': '$user_agent'}";
+        $msg = "Request coming {'client-IP': '$client_ip', 'request-method': '$request_method', 'url': '$request_url', 'user-agent': '$user_agent'}";
         Log::build([
             'driver' => 'single',
             'path' => storage_path('logs/app.log'),
