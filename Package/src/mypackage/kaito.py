a46d130da0c3f587ed5887716e4b5a0e084ea402
Kaito <kaitok57a01@gmail.com>
2022-12-06 23:53:11 +0700

    feat:add python publish package

---
+++ Package/src/mypackage/kaito.py
@@ -0,0 +1,43 @@
+# define size n = even only
+n = 8
+ 
+m = n+1
+ 
+for i in range(n//2-1):
+    for j in range(m):
+         
+        if i == n//2-2 and (j == 0 or j == m-1):
+            print("*", end=" ")
+             
+        elif j <= m//2 and ((i+j == n//2-3 and j <= m//4) \
+                            or (j-i == m//2-n//2+3 and j > m//4)):
+            print("*", end=" ")
+             
+        elif j > m//2 and ((i+j == n//2-3+m//2 and j < 3*m//4) \
+                           or (j-i == m//2-n//2+3+m//2 and j >= 3*m//4)):
+            print("*", end=" ")
+             
+        else:
+            print(" ", end=" ")
+    print()
+ 
+# loops for lower part
+for i in range(n//2-1, n):
+    for j in range(m):
+         
+        if (i-j == n//2-1) or (i+j == n-1+m//2):
+            print('*', end=" ")
+             
+        elif i == n//2-1:
+             
+            if j == m//2-1 or j == m//2+1:
+                print(' ', end=" ")
+            elif j == m//2:
+                print(' ', end=" ")
+            else:
+                print(' ', end=" ")
+                 
+        else:
+            print(' ', end=" ")
+             
+    print()
